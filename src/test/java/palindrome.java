8d3fac54ac080daec47f2e5b06c17588180974d8
aloks <aloksingh7587@gmail.com>
2026-02-01 18:23:01 +0530

    Initial Commit

---
+++ src/test/java/palindrome.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class palindrome {
+
+    public static boolean isPalindrome(String str) {
+
+        str = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        if (isPalindrome(str)) {
+            System.out.println(str + " is a palindrome.");
+        } else {
+            System.out.println(str + " is not a palindrome.");
+        }
+    }
+}
